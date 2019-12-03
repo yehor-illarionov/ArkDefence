d2bf79b87dbdb931d2bb6f974ccc8142da7d772c
Illarionov Yegor <egorliberty@gmail.com>
2019-12-03 18:32:32 +0200

    ref

--- WebApplication1/Handlers/TenantCreatedHandler.cs
+++ WebApplication1/Handlers/TenantCreatedHandler.cs
@@ -1,12 +1,9 @@
 ﻿using Enexure.MicroBus;
-using Finbuckle.MultiTenant;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using WebApplication1.Data;
 using WebApplication1.Messaging.Events;
 
 namespace WebApplication1.Handlers
@@ -25,38 +22,6 @@ namespace WebApplication1.Handlers
             log.LogInformation($"Tenant created with id: '{@event.Id}' on: '{@event.CreationTime.ToString()}'");
             // throw new NotImplementedException();
             return Task.CompletedTask;
-        }
-    }
-
-    public class TenantCreatedNotDefaultHandler : IEventHandler<TenantCreated>
-    {
-        private readonly ILogger log;
-        private readonly IMultiTenantStore context;
-
-        public TenantCreatedNotDefaultHandler(ILogger<TenantCreatedHandler> log, IMultiTenantStore context)
-        {
-            this.log = log ?? throw new ArgumentNullException(nameof(log));
-            this.context = context ?? throw new ArgumentNullException(nameof(context));
-        }
-
-        public async Task Handle(TenantCreated @event)
-        {
-            //TODO exceptions
-            log.LogInformation($"NonDefault Tenant created with id: '{@event.Id}' on: '{@event.CreationTime.ToString()}'");
-            // throw new NotImplementedException();
-            var tenantInfo=await context.TryGetAsync(@event.Id);
-            if(tenantInfo is null)
-            {
-                log.LogInformation($"Failed to prepare database for Tenant with id: '{@event.Id}' on: '{@event.CreationTime.ToString()}'");
-                return;
-            }
-            var builder = new DbContextOptionsBuilder<NextAppContext>().UseNpgsql(tenantInfo.ConnectionString);
-            using(var db = new NextAppContext(tenantInfo, builder.Options))
-            {
-                db.Database.EnsureCreated();
-                db.Database.Migrate();
-            }
-            log.LogInformation($"Prepared database for Tenant with id: '{@event.Id}' on: '{@event.CreationTime.ToString()}'");
         }
     }
 }
